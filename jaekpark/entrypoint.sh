ea67e486fcc6a86266fcddc841474f569932f781
jaekpark <jaekpark@student.42seoul.kr>
2021-06-07 22:53:22 +0900

    add new web file

---
+++ jaekpark/entrypoint.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo apt -y remove cmdtest
+sudo apt -y remove yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install yarn -y
+
+yarn install && yarn build
+
+yarn start
